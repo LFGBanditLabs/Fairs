99569d7fca18dc54561aba6cee741f1432f8837a
lynn <okolo.ebelelynda@gmail.com>
2025-11-05 18:33:12 +0100

    Integrate Web3 providers in _app.js

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,5 +1,14 @@
 import '../styles/globals.css'
+import { WagmiProvider } from 'wagmi'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { config, queryClient } from '../config/web3'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </WagmiProvider>
+  )
 }
